4e657f3bc36b14da7447b49ed8f0680bd5c6a625
Jason Kleban <y2kjbk@gmail.com>
2011-11-28 15:45:54 -0500

    Update patch-src-fakechroot.c

--- patch-src-fakechroot.c
+++ patch-src-fakechroot.c
@@ -1,14 +1,14 @@
---- src/fakechroot.c.orig	2011-09-07 01:02:03.000000000 -0400
-+++ src/fakechroot.c	2011-11-16 15:55:30.000000000 -0500
-@@ -11,12 +11,16 @@
- # endif
- 
+--- src/fakechroot.c.orig 2011-09-07 01:02:03.000000000 -0400
++++ src/fakechroot.c	2011-11-18 09:57:05.000000000 -0500
+@@ -13,10 +13,16 @@
  static char curdir[MAXPATHLEN];
+ static char *chroot_base;
+ static size_t chroot_len;
++#ifdef WITH_VIRTUALDIRS
 +static char curdir_virtual[MAXPATHLEN];
 +static char dir_workspace[MAXPATHLEN];
 +static char dir_storage[MAXPATHLEN];
- static char *chroot_base;
- static size_t chroot_len;
++#endif
  
  int fakechroot(const char *path)
  {
@@ -18,11 +18,12 @@
      
      if (path == NULL || *path == 0) {
  # ifdef EINVAL
-@@ -50,7 +54,46 @@
+@@ -50,7 +56,48 @@
  # endif
          return -1;
      }
 -    
++#ifdef WITH_VIRTUALDIRS
 +    for (i=0; i<num_virtualdirs; i++) {
 +	curr_virtualdir = virtualdirs[i]->virtualdir_base;
 +	simplify(curr_virtualdir);
@@ -63,43 +64,55 @@
 +            return -1;
 +        }
 +    }
++#endif
      return 0;
  }
  
-@@ -62,10 +105,10 @@
+@@ -62,10 +109,17 @@
      if (chroot_base == NULL) {
          return getcwd(dir, size);
      }
--    if (strncmp(curdir, chroot_base, chroot_len) != 0) {
++#ifdef WITH_VIRTUALDIRS
 +    if (strncmp(curdir_virtual, chroot_base, chroot_len) != 0) {
++        abort();
++    }
++    curdirchr = curdir_virtual + chroot_len;
++#else
+     if (strncmp(curdir, chroot_base, chroot_len) != 0) {
          abort();
      }
--    curdirchr = curdir + chroot_len;
-+    curdirchr = curdir_virtual + chroot_len;
+     curdirchr = curdir + chroot_len;
++#endif
      s = strlen(curdirchr);
      if (s <= (size_t) 0U) {
  # ifdef EFAULT
-@@ -97,11 +140,12 @@
+@@ -97,11 +151,19 @@
  {
      char *sl;     
      size_t curdirlen;
++#ifdef WITH_VIRTUALDIRS
 +    int i;
      
--    if ((curdirlen = strlen(curdir)) >= sizeof_curdirtmp) {
 +    if ((curdirlen = strlen(curdir_virtual)) >= sizeof_curdirtmp) {
++        return -1;
++    }
++    memcpy(curdirtmp, curdir_virtual, curdirlen + (size_t) 1U);
++#else
+     if ((curdirlen = strlen(curdir)) >= sizeof_curdirtmp) {
          return -1;
      }
--    memcpy(curdirtmp, curdir, curdirlen + (size_t) 1U);
-+    memcpy(curdirtmp, curdir_virtual, curdirlen + (size_t) 1U);
+     memcpy(curdirtmp, curdir, curdirlen + (size_t) 1U);
++#endif
      simplify(curdirtmp);
      loop:
      if (dir[0] == '.' && dir[1] == '.' &&
-@@ -153,7 +197,19 @@
+@@ -153,7 +215,19 @@
          memcpy(curdirtmp + curdirtmplen + 1U, dir, dirlen);
      }     
      simplify(curdirtmp);
+-    
++#ifdef WITH_VIRTUALDIRS
 +    memcpy(dir_storage, curdirtmp, sizeof dir_storage);
-     
 +    for(i=0; i<num_virtualdirs; i++) {
 +	if (strncmp(curdirtmp + chroot_len, virtualdirs[i]->virtualdir_name, virtualdirs[i]->virtualdir_name_len) == 0) {
 +	    memcpy(dir_workspace, virtualdirs[i]->virtualdir_base, virtualdirs[i]->virtualdir_base_len);
@@ -110,16 +123,18 @@
 +	    return 0;
 +	}
 +    }
-+
++#endif
      return 0;
  }
  
-@@ -175,7 +231,7 @@
+@@ -175,7 +249,9 @@
          return -1;
      }
      memcpy(curdir, curdirtmp, curdirtmplen + (size_t) 1U);
 -    
++#ifdef WITH_VIRTUALDIRS
 +    memcpy(curdir_virtual, dir_storage, sizeof dir_storage + (size_t) 1U);
++#endif
      return 0;
  }
  
